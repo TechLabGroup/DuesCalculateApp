62838c3fb9ddcd47b54e38acdc2a5fa35f038ff8
nagasakiTechLab <nagasaki@TechLab>
2017-07-20 22:50:39 +0900

    Initial Commit

---
+++ DuesCalculateApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DuesCalculateApp
+//
+//  Created by 長崎　二郎 on 2017/07/20.
+//  Copyright © 2017年 TechLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
