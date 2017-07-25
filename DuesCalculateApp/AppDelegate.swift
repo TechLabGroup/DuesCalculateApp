7c024c1d5a9f29a0b842f3640e16af3c6f7a0cda
nagasakiTechLab <nagasaki@TechLab>
2017-07-25 20:58:39 +0900

    Storyboard削除、アプリ起動時にPartyListViewControllerを表示する設定

--- DuesCalculateApp/AppDelegate.swift
+++ DuesCalculateApp/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        // アプリ起動時はPartyListViewControllerを表示する
+        let vc = PartyListViewController()
+        let navi = UINavigationController(rootViewController: vc)
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navi
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
