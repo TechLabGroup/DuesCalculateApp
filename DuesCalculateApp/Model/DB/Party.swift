8cb988e189a15fa7a307e9c2bc346cc13204bd93
Hiroyuki Tanino <tanino.hiroyuki@tis.co.jp>
2017-08-17 20:02:28 +0900

    飲み会オブジェクトの新規作成

---
+++ DuesCalculateApp/Model/DB/Party.swift
@@ -0,0 +1,23 @@
+//
+//  Party.swift
+//  DuesCalculateApp
+//
+//  Created by Tanino-Sato on 2017/08/10.
+//  Copyright © 2017年 TechLab. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+final class Party: Object {
+    dynamic var partyId : Int = 0 // 飲み会ID
+    dynamic var partyName : String = "" // 飲み会名
+//    dynamic var date : Date //開催日
+    dynamic var date : String = "" // 開催日（仮）
+    dynamic var totalAmount : Int = 0 // 合計金額
+    
+    //主キーに飲み会IDを設定
+    override static func primaryKey() -> String? {
+        return "partyId"
+    }
+}
