ec14daec6581714f5af2a07c8412026f0b1219de
YusukeUenaka <uenaka.yusuke@tis.co.jp>
2017-09-12 21:48:28 +0900

    ダイアログ表示。TableViewのパーツ配置。

---
+++ DuesCalculateApp/View/PartyDetail/CustomMemberCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomMemberCell.swift
+//  DuesCalculateApp
+//
+//  Created by TechLab.1 on 2017/09/11.
+//  Copyright © 2017年 TechLab. All rights reserved.
+//
+
+import UIKit
+
+class CustomMemberCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
