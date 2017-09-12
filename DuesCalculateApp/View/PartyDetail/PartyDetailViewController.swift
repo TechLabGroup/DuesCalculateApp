b46d55090cc10e6b9069b18451581ba518acfbef
YusukeUenaka <uenaka.yusuke@tis.co.jp>
2017-09-12 21:48:28 +0900

    ダイアログ表示。TableViewのパーツ配置。

--- DuesCalculateApp/View/PartyDetail/PartyDetailViewController.swift
+++ DuesCalculateApp/View/PartyDetail/PartyDetailViewController.swift
@@ -6,18 +6,53 @@
 //
 
 import UIKit
+import RealmSwift
 
 class PartyDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
+        
+        //NavigationBarのタイトル右上に「+」を表示
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add, target: self, action: #selector(editMember) )
+        
+        //NavigationBarのタイトル名取得
+        self.navigationItem.title = "test" /*ダミー*/
     }
 
+    //精算者選択画面へ遷移
+    func editMember() {
+        let vc = MemberSelectViewController()
+        let nc = UINavigationController(rootViewController: vc)
+        present(nc, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func alertPush(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "確認!", message: "未払い者に一斉通知します。よろしければOKを選んでください。", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "CANCEL", style: .default, handler: nil)
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(okAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    @IBAction func warikanPush(_ sender: Any) {
+        let alertController = UIAlertController(title: "確認!", message: "金額が自動的に割り勘で再計算されます。よろしければOKを選んでください。", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "CANCEL", style: .default, handler: nil)
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(okAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
     
 
