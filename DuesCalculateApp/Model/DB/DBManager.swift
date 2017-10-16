8ba8f1c07868551b358a9956aa4da56cc578eb89
YusukeUenaka <uenaka.yusuke@tis.co.jp>
2017-10-16 12:16:20 +0900

    ・スイッチを押した時、セルの背景色変更処理を追記 ・スイッチを押した時、Realmを更新する処理を追記 ・通知ボタンの枠を丸角枠に変更 ・アイコンを指定画像に追記

--- DuesCalculateApp/Model/DB/DBManager.swift
+++ DuesCalculateApp/Model/DB/DBManager.swift
@@ -10,4 +10,134 @@ import RealmSwift
 
 final class DBManager {
     
+    /// <#Description#>
+    /// 飲み会作成
+    ///
+    /// - Parameters:
+    ///   - partyName: <#partyName description#>
+    ///   - partyDate: <#partyDate description#>
+    ///   - totalAmount: <#totalAmount description#>
+    /// - Returns: <#return value description#>
+    public static func createParty(partyName: String, partyDate: String, totalAmount: Int) -> Bool {
+        let realm = try! Realm()
+        
+        let party = Party()
+        party.partyId = incrementID()
+        party.partyName = partyName
+        party.date = partyDate
+        party.totalAmount = totalAmount
+        
+        try! realm.write {
+            realm.add(party)
+        }
+        return true
+    }
+    
+    /// <#Description#>
+    /// メンバー登録
+    ///
+    /// - Parameters:
+    ///   - partyId: <#partyId description#>
+    ///   - memberName: <#memberName description#>
+    ///   - mailAddress: <#mailAddress description#>
+    ///   - paymentCompleteFlag: <#paymentCompleteFlag description#>
+    ///   - paymentAmount: <#paymentAmount description#>
+    /// - Returns: <#return value description#>
+    public static func entryMember(partyId: Int, memberName: String, mailAddress: String, paymentCompleteFlag: Int, paymentAmount: Int) -> Bool {
+        let realm = try! Realm()
+        
+        let member = Member()
+        member.partyId = partyId
+        member.memberName = memberName
+        member.mailAddress = mailAddress
+        member.paymentCompleteFlag = paymentCompleteFlag
+        member.paymentAmount = paymentAmount
+        
+        try! realm.write {
+            realm.add(member)
+        }
+        return true
+    }
+    
+    /// <#Description#>
+    /// partyIdのシーケンス
+    ///
+    /// - Returns: <#return value description#>
+    private static func incrementID() -> Int {
+        let realm = try! Realm()
+        return (realm.objects(Party.self).max(ofProperty: "partyId") as Int? ?? 0) + 1
+    }
+    
+    /// <#Description#>
+    /// 飲み会一覧検索
+    ///
+    /// - Returns: <#return value description#>
+    public static func searchParty() -> [Party] {
+        let realm = try! Realm()
+        let party = realm.objects(Party.self)
+        return Array(party)
+    }
+    
+    /// <#Description#>
+    /// 飲み会名称検索
+    ///
+    /// - Parameter partyId: <#partyId description#>
+    /// - Returns: <#return value description#>
+    public static func searchPartyName(partyId: Int) -> String {
+        let realm = try! Realm()
+        let party = realm.objects(Party.self).filter("partyId == %@", partyId)
+        var partyName = ""
+        for name in party {
+            partyName = name.partyName
+        }
+        return partyName
+    }
+    
+    /// <#Description#>
+    /// 飲み会参加者一覧検索
+    ///
+    /// - Parameter partyId: <#partyId description#>
+    /// - Returns: <#return value description#>
+    public static func searchAllMember(partyId : Int) -> [Member] {
+        let realm = try! Realm()
+        let member = realm.objects(Member.self).filter("partyId == %@", partyId)
+        return Array(member)
+    }
+    
+    
+    /// <#Description#>
+    /// 支払い完了フラグに更新
+    ///
+    /// - Parameters:
+    ///   - partyId: <#partyId description#>
+    ///   - memberName: <#memberName description#>
+    ///   - paymentCompleteFlag: <#paymentCompleteFlag description#>
+    public static func updateMemberFinish(partyId: Int, memberName: String, paymentCompleteFlag: Int) {
+        let realm = try! Realm()
+        let member = realm.objects(Member.self).filter("memberName = %@ && partyId == %@", memberName, partyId).first
+        try! realm.write ({ () -> Void in
+            member?.paymentCompleteFlag = 1 })
+    }
+    
+    /// <#Description#>
+    /// 支払い完了フラグを解除
+    ///
+    /// - Parameters:
+    ///   - partyId: <#partyId description#>
+    ///   - memberName: <#memberName description#>
+    ///   - paymentCompleteFlag: <#paymentCompleteFlag description#>
+    public static func repositupdateMember(partyId: Int, memberName: String, paymentCompleteFlag: Int) {
+        let realm = try! Realm()
+        let member = realm.objects(Member.self).filter("memberName = %@ && partyId == %@", memberName, partyId).first
+        try! realm.write ({ () -> Void in
+            member?.paymentCompleteFlag = 0 })
+    }
+    
+    //合計金額
+
+    
+    //集計金額
+    
+    //残り人数
+    
 }
--- DuesCalculateApp/Model/DB/Party.swift
+++ DuesCalculateApp/Model/DB/Party.swift
@@ -10,11 +10,11 @@ import Foundation
 import RealmSwift
 
 final class Party: Object {
-    dynamic var partyId : Int = 0 // 飲み会ID
-    dynamic var partyName : String = "" // 飲み会名
-//    dynamic var date : Date //開催日
-    dynamic var date : String = "" // 開催日（仮）
-    dynamic var totalAmount : Int = 0 // 合計金額
+    dynamic var partyId: Int = 0 // 飲み会ID
+    dynamic var partyName: String = "" // 飲み会名
+    //    dynamic var date : Date //開催日
+    dynamic var date: String = "" // 開催日（仮）
+    dynamic var totalAmount: Int = 0 // 合計金額
     
     //主キーに飲み会IDを設定
     override static func primaryKey() -> String? {
