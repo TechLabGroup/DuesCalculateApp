d14afa7707c64da3b5022896f05deaab96f7cb74
YusukeUenaka <uenaka.yusuke@tis.co.jp>
2017-10-16 12:16:20 +0900

    ・スイッチを押した時、セルの背景色変更処理を追記 ・スイッチを押した時、Realmを更新する処理を追記 ・通知ボタンの枠を丸角枠に変更 ・アイコンを指定画像に追記

--- DuesCalculateApp/View/PartyDetail/CustomMemberCell.swift
+++ DuesCalculateApp/View/PartyDetail/CustomMemberCell.swift
@@ -8,17 +8,45 @@
 
 import UIKit
 
-class CustomMemberCell: UITableViewCell {
+class CustomMemberCell: UITableViewCell  {
 
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var memberCell: UIView!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var sw: UISwitch!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    /// <#Description#>
+    /// セルの背景色を変更
+    ///
+    /// - Parameter sender: <#sender description#>
+    @IBAction func changeCellColor(_ sender: UISwitch) {
+        if sw.isOn {
+            memberCell.backgroundColor = UIColor.gray
+        } else {
+            memberCell.backgroundColor = UIColor.white
+        }
+        
     }
+    
+
+//    
+//    /// <#Description#>
+//    ///
+//    ///
+//    /// - Parameters:
+//    ///   - selected: <#selected description#>
+//    ///   - animated: <#animated description#>
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
     
 }
--- DuesCalculateApp/View/PartyDetail/PartyDetailViewController.swift
+++ DuesCalculateApp/View/PartyDetail/PartyDetailViewController.swift
@@ -8,22 +8,128 @@
 import UIKit
 import RealmSwift
 
-class PartyDetailViewController: UIViewController {
+class PartyDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var PartyDetailTable: UITableView!
+    
+    @IBOutlet weak var remainMember: UILabel!
+    @IBOutlet weak var collectPrice: UILabel!
+    @IBOutlet weak var surplusPrice: UILabel!
+    @IBOutlet weak var sumPrice: UILabel!
+    var members: [Member]?
+    var pId: Int = 0
+    var pName: String = "テスト"
+    var memberName: String = "test"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        test()
 
         // Do any additional setup after loading the view.
         
         //NavigationBarのタイトル右上に「+」を表示
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add, target: self, action: #selector(editMember) )
-        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(editMember) )
         //NavigationBarのタイトル名取得
-        self.navigationItem.title = "test" /*ダミー*/
+        pName = DBManager.searchPartyName(partyId: pId)
+        members = DBManager.searchAllMember(partyId: pId)
+        //var partyname = DBManager.searchPartyName(partyId: 4)
+        //self.navigationItem.title = partyname
+        self.navigationItem.title = pName
+        
+        //合計金額
+        sumPrice.text = "test"
+        //残り金額
+        surplusPrice.text = "test"
+        //実績金額
+        collectPrice.text = "test"
+        //残り人数
+        remainMember.text = "test"
+
+        //自作セルをテーブルビューに登録する。
+        let memberCell = UINib(nibName: "CustomMemberCell", bundle: nil)
+        PartyDetailTable.register(memberCell, forCellReuseIdentifier: "CustomMemberCell")
+        
+    }
+    
+    /// <#Description#>
+    /// テストデータ投入
+    func test() {
+        let realm = try! Realm()
+        let testTable = Party()
+        testTable.partyId = 0
+        testTable.partyName = "test"
+        testTable.date = "20170914"
+        testTable.totalAmount = 50000
+        try! realm.write {
+            realm.add(testTable, update: true)
+        }
+        let testTable1 = Member()
+        testTable1.partyId = 0
+        testTable1.memberName = "藤井隆司"
+        testTable1.mailAddress = "test@tis.co.jp"
+        testTable1.paymentCompleteFlag = 1
+        testTable1.paymentAmount = 500
+        try! realm.write {
+            realm.add(testTable1, update: true)
+        }
+    }
+    
+    
+    
+    //TableView
+    
+    /// <#Description#>
+    /// セルの個数を指定するデリゲートメソッド（必須）
+    ///
+    /// - Parameters:
+    ///   - tableView: <#tableView description#>
+    ///   - section: <#section description#>
+    /// - Returns: <#return value description#>
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 通常は引数のセクションで分岐して値を返却する
+        return members!.count
+    }
+    
+    
+    /// <#Description#>
+    /// セルの値と背景色を設定。
+    ///
+    /// - Parameters:
+    ///   - tableView: <#tableView description#>
+    ///   - indexPath: <#indexPath description#>
+    /// - Returns: <#return value description#>
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomMemberCell", for: indexPath) as! CustomMemberCell
+        // Configure the cell...
+        var paymentAmount: Int = 0
+        var finishFlg: Int = 0
+        cell.name.text = members?[indexPath.row].memberName
+        paymentAmount = members![indexPath.row].paymentAmount
+        cell.price.text = String(paymentAmount)
+        finishFlg = members![indexPath.row].paymentCompleteFlag
+        if finishFlg == 1 {
+            cell.backgroundColor = UIColor.white
+            cell.sw.isOn = false
+        } else {
+            cell.backgroundColor = UIColor.gray
+            cell.sw.isOn = true
+        }
+        
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
     }
 
-    //精算者選択画面へ遷移
+
+
+    /// <#Description#>
+    /// 精算者選択画面へ遷移
     func editMember() {
         let vc = MemberSelectViewController()
         let nc = UINavigationController(rootViewController: vc)
@@ -34,6 +140,12 @@ class PartyDetailViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    /// <#Description#>
+    /// 通知ボタン押下時のダイアログ
+    ///
+    /// - Parameter sender: <#sender description#>
     @IBAction func alertPush(_ sender: UIButton) {
         let alertController = UIAlertController(title: "確認!", message: "未払い者に一斉通知します。よろしければOKを選んでください。", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
@@ -44,6 +156,11 @@ class PartyDetailViewController: UIViewController {
         
         present(alertController, animated: true, completion: nil)
     }
+    
+    /// <#Description#>
+    /// ワリカンボタン押下時のダイアログ
+    ///
+    /// - Parameter sender: <#sender description#>
     @IBAction func warikanPush(_ sender: Any) {
         let alertController = UIAlertController(title: "確認!", message: "金額が自動的に割り勘で再計算されます。よろしければOKを選んでください。", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
@@ -55,6 +172,9 @@ class PartyDetailViewController: UIViewController {
         present(alertController, animated: true, completion: nil)
     }
     
+    
+    
+    
 
     /*
     // MARK: - Navigation
@@ -65,5 +185,6 @@ class PartyDetailViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
 
 }
